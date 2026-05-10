9582d2f56b60ff5a67d3915f1f0c38784110fb3d
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2026-05-11 07:39:54 +0900

    Try defining drawing

--- FourColorTheorem/Basic.lean
+++ FourColorTheorem/Basic.lean
@@ -1,1 +1,18 @@
-def hello := "world"
+import Mathlib.Topology.MetricSpace.Pseudo.Defs
+import Mathlib.Analysis.InnerProductSpace.PiL2
+
+set_option linter.style.longLine false
+
+abbrev R3 := EuclideanSpace ℝ (Fin 3)
+abbrev S2 := Metric.sphere (0 : R3) 1
+
+def IsDrawingEdge (e : Set S2) : Prop :=
+  ∃ f : unitInterval ≃ₜ closure e, closure e \ e = {↑(f 0), ↑(f 1)}
+
+structure Drawing where
+  univ : Set S2
+  univ_closed : IsClosed univ
+  vertices : Finset S2
+  vertices_in_univ : ↑vertices ⊆ univ
+  edges_finite : Finite (ZerothHomotopy ↑(univ \ vertices))
+  edges_are_line : ∀ x ∈ (univ \ vertices), IsDrawingEdge (pathComponentIn (univ \ vertices) x)
